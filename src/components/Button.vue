15469ae6b6657380eed5100f3786847392f54539
MoritzBernhofer <114606708+MoritzBernhofer@users.noreply.github.com>
2023-02-05 00:50:20 +0100

    Hello there

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+    <button @click="onClick()" :style="{ background: color }" class="btn">
+        {{ text }}
+    </button>
+</template>
+
+<script>
+export default {
+    name: "Button",
+    props: {
+        text: {
+            type: String,
+            required: true,
+        },
+        color: {
+            type: String,
+            default: "steelblue",
+        },
+    },
+    methods: {
+        onClick() {
+            console.log("Hello there");
+        },
+    },
+};
+</script>
